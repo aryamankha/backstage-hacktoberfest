ab6ca75cc8574bfdcb686a67ec4cc25653f44404
Harsh Gupta <harsh.gupta2211@gmail.com>
2022-10-10 08:45:55 +0530

    Added Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Hero from "../components/hero";
 import Drops from "../components/drops";
 import Faq from "../components/faq";
+import Header from "./header";
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
+      <Header/>
       <Hero/>
       <Drops/>
       <Faq />
