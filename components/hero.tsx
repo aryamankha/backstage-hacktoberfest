aea524de2afb997f3ed4265507f9767e3e84cf20
zfir <zafir.heerah@spoonconsulting.com>
2022-10-10 21:26:23 +0400

    Format TSX Files

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <div className="object-cover bg-[url('../public/images/artist-concert.jpg')] bg-no-repeat bg-center bg-cover h-screen flex justify-center items-center">
       <div className="flex flex-col items-center gap-16 w-7/12 text-white">
         <h1 className="self-center text-center font-bold drop-shadow-xl text-6xl">
-          Be the first to discover a star. 
+          Be the first to discover a star.
         </h1>
         <p className="self-center text-center drop-shadow-xl">
           <strong>246,900</strong> Users. <strong>3,904</strong> Artists.{" "}
@@ -15,7 +15,6 @@ export default function Hero() {
         <button className="text-[1.1rem] mx-[1rem] bg-[#485ED1] px-5 py-2 rounded-lg hover:bg-[#364aaf]">
           Sign Up
         </button>
-        
       </div>
     </div>
   );
