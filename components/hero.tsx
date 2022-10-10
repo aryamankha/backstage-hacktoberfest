2ff8fbce3dc8947591717172c15769ddc2a0b354
aryamankha <aryamank@princeton.edu>
2022-10-10 13:22:23 -0400

    Edit copy on landing

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,8 +5,8 @@ export default function Hero() {
   return (
     <div className="object-cover bg-[url('../public/images/artist-concert.jpg')] bg-no-repeat bg-center bg-cover h-screen flex justify-center items-center">
       <div className="flex flex-col items-center gap-16 w-7/12 text-white">
-        <h1 className="self-center text-center text-4xl font-bold drop-shadow-xl">
-          Our tagline goes here. It&apos;s really cool and eye-catching.
+        <h1 className="self-center text-center font-bold drop-shadow-xl text-6xl">
+          Be the first to discover a star. 
         </h1>
         <p className="self-center text-center drop-shadow-xl">
           <strong>246,900</strong> Users. <strong>3,904</strong> Artists.{" "}
