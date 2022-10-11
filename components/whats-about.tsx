cc787e945004c5d5def25806f2f9909298d33e50
redxzeta <xzetanzero@gmail.com>
2022-10-10 18:37:02 -0700

    feat: about images

--- components/whats-about.tsx
+++ components/whats-about.tsx
@@ -1,11 +1,17 @@
-type WhatIsItAboutType = { title: string };
+import Image, { StaticImageData } from "next/image";
+import Win from "../public/images/win.jpg";
+import Create from "../public/images/create.jpg";
+import Buy from "../public/images/buy.jpg";
 
-function Card({ title }: WhatIsItAboutType) {
+type WhatIsItAboutType = { title: string; img: StaticImageData };
+
+function Card({ title, img }: WhatIsItAboutType) {
   return (
     <div
-      className="border border-white rounded-xl p-4 flex justify-center items-end"
+      className="border border-white rounded-xl p-4 flex justify-center items-end flex-col"
       style={{ width: "323px", height: "323px" }}
     >
+      <Image src={img} alt={title} />
       <h2 className="text-4xl font-light">{title}</h2>
     </div>
   );
@@ -21,17 +27,13 @@ export default function WhatIsItAbout() {
         What&apos;s it all about?
       </h1>
       <div className="flex justify-between w-full mb-20">
-        <Card
-          title="Artists Drop"
-        />
-        <Card
-          title="Fans Buy"
-        />
-        <Card title="Success Shared" />
+        <Card title="Artists Drop" img={Win} />
+        <Card title="Fans Buy" img={Buy} />
+        <Card title="Success Shared" img={Create} />
       </div>
       <p className="text-2xl font-normal">
-        By letting fans buy into their favorite artists&apos; success, they can help
-        the creators they love succeed while sharing in that success.
+        By letting fans buy into their favorite artists&apos; success, they can
+        help the creators they love succeed while sharing in that success.
       </p>
     </div>
   );
