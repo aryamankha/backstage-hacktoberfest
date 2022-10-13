3b257ec8bf0dd151dcb67bec0c94cdd21397cc09
Partha <pparthapaul11@gmail.com>
2022-10-14 02:46:49 +0530

    drops section fixed and types added

--- components/drops.tsx
+++ components/drops.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Link from "next/link";
 
 export default function Drops() {
@@ -9,42 +10,31 @@ export default function Drops() {
         </h1>
         <div className="bg-purple-500 h-1 mt-2 w-64 md:w-96 ml-5"></div>
       </div>
-      <div className="card-container flex md:flex-row flex-col gap-5 h-96">
-        <div className="cont">
-          <div className="card1 h-64 w-64 border-solid border-black rounded-lg border-2 mt-8 mb-5 ml-5">
-            <h1 className="text-black text-2xl font-bold font-sans m-3 w-36">
-              NFT Title 1
-            </h1>
-            <p className="text-black mt-36 ml-3">Artist Name</p>
-          </div>
-          <button className="self-center text-center py-0.5 rounded bg-purple-500 px-2.5 ml-5">
-            {" "}
-            <Link href="/starprofile"> See Artist</Link>
-          </button>
-        </div>
-        <div className="cont">
-          <div className="card2 h-64 w-64 border-solid border-black rounded-lg border-2 mt-8 mb-5 ml-5">
-            <h1 className="text-black text-2xl font-bold font-sans m-3 w-36">
-              NFT Title 2
-            </h1>
-            <p className="text-black mt-36 ml-3">Artist Name</p>
-          </div>
-          <button className="self-center text-center py-0.5 rounded bg-purple-500 px-2.5 ml-5">
-            <Link href="/starprofile"> See Artist</Link>
-          </button>
-        </div>
-        <div className="cont">
-          <div className="card3 h-64 w-64 border-solid border-black rounded-lg border-2 mt-8 mb-5 ml-5">
-            <h1 className="text-black text-2xl font-bold font-sans m-3 w-36">
-              NFT Title 3
-            </h1>
-            <p className="text-black mt-36 ml-3">Artist Name</p>
-          </div>
-          <button className="self-center text-center py-0.5 rounded bg-purple-500 px-2.5 ml-5">
-            <Link href="/starprofile"> See Artist</Link>
-          </button>
-        </div>
+      <div className="card-container flex md:flex-row flex-col gap-5">
+       <DropCard title="NFT Title  1" artist="Artist Name" link="/startProfile" /> 
+       <DropCard title="NFT Title  2" artist="Artist Name" link="/startProfile" /> 
       </div>
     </div>
   );
+}
+
+interface DropProps {
+  title: string;
+  artist: string;
+  link: string;
+}
+
+const DropCard:React.FC<DropProps> = ({...props}:DropProps)=>{
+
+        return <div className="cont">
+          <div className="card3 h-64 w-64 border-solid border-black rounded-lg border-2 mt-8 mb-5 ml-5">
+            <h2 className="text-black text-2xl font-bold font-sans m-3 w-36">
+              {props.title}
+            </h2>
+            <p className="text-black mt-36 ml-3">{props.artist}</p>
+          </div>
+          <button className="self-center text-center py-0.5 rounded bg-purple-500 px-2.5 ml-5">
+            <Link href={props.link}> See Artist</Link>
+          </button>
+        </div>
 }
--- components/faq.tsx
+++ components/faq.tsx
@@ -1,57 +1,61 @@
-type FaqContainerType = { title: string; text: string };
+import React from "react";
 
-function FaqContainer({ title, text }: FaqContainerType) {
+interface FaqContainerType {
+  title: string;
+  text: string;
+}
+
+const FaqContainer: React.FC<FaqContainerType> = ({
+  ...props
+}: FaqContainerType) => {
   return (
     <>
-      <h2 className="text-4xl font-light mb-4">{title}</h2>
-      <p className="text-2xl font-normal mb-10">{text}</p>
+      <h2 className="text-4xl font-light mb-4">{props.title}</h2>
+      <p className="text-2xl font-normal mb-10">{props.title}</p>
     </>
   );
-}
+};
 
 export default function Faq() {
   return (
-    <div className="my-2">
+    <div className="my-5">
       <div className="bg-black py-14 px-2 sm:px-44">
-        <h1
-          className="text-4xl font-black"
-          style={{ maxWidth: "550px" }}
-        >
+        <h1 className="text-4xl font-black" style={{ maxWidth: "550px" }}>
           FAQ
         </h1>
-      <div className="bg-purple-500 h-1 mt-2 w-28 md:w-96"></div>
+        <div className="bg-purple-500 h-1 mt-2 w-28 md:w-96"></div>
       </div>
-    
-    <div className="px-4 sm:px-20 md:px-44">
-      <FaqContainer
+
+      <div className="px-4 sm:px-20 md:px-44">
+        <FaqContainer
           title="What is Backstage?"
           text="Backstage lets fans support their favorite stars and share in their
           success by buying NFT tokens and participating in star-led communities."
         />
-      <FaqContainer
-        title="When I buy a star's NFT, what do I get?"
-        text="Each NFT has its own set of unique benefits chosen by the star
+        <FaqContainer
+          title="When I buy a star's NFT, what do I get?"
+          text="Each NFT has its own set of unique benefits chosen by the star
         themselves. All NFTs come with access to that star's private
         Discord, but other potential benefits include access to exclusive
         merchandise, first notification when event tickets drop, and
         personalized content from the star!"
-      />
-      <FaqContainer
-        title="What makes Backstage unique?"
-        text="Backstage is focused on helping early-stage stars build their brand from
+        />
+        <FaqContainer
+          title="What makes Backstage unique?"
+          text="Backstage is focused on helping early-stage stars build their brand from
         the ground up. We're the only platform in the business of actually
         getting stars discovered by agents, talent scouts, and industry veterans
         that can take their career to the next level. For you as a fan, this
         means supporting your favorite stars on Backstage gets them one step
         closer to hitting it big."
-      />
-      <FaqContainer
-        title="How does Backstage let me share in the success of my stars?"
-        text="Each NFT minted through Backstage is ordered, which means if you buy an
+        />
+        <FaqContainer
+          title="How does Backstage let me share in the success of my stars?"
+          text="Each NFT minted through Backstage is ordered, which means if you buy an
         NFT for an artist and they become wildly successful, the value of your
         NFT will skyrocket!"
-      />
-    </div>
+        />
+      </div>
     </div>
   );
 }
