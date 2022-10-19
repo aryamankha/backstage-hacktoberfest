bbf4141544bdc07ca4a74fc41aebfaeed8e4affc
Partha <pparthapaul11@gmail.com>
2022-10-19 11:07:08 +0530

    latest drops made swipable carousal

--- components/drops.tsx
+++ components/drops.tsx
@@ -1,14 +1,47 @@
 import Link from "next/link";
-import Image from 'next/image';
-import { FC } from 'react';
+import Image from "next/image";
+import { FC } from "react";
+import { useState,useEffect } from "react";
 
-import { NFT } from '../types';
+//swiper imports 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+//swiper used for swipable carousal
+
+import { NFT } from "../types";
 
 interface DropsProps {
-  nfts: NFT[]
-};
+  nfts: NFT[];
+}
 
-const Drops:FC<DropsProps> = ({ nfts }) => {
+const Drops: FC<DropsProps> = ({ nfts }) => {
+  const [slidesPerView, setSlidesPerView] = useState(0);
+
+  useEffect(() => {
+    switch (process.browser) {
+      case window.innerWidth<540:
+        setSlidesPerView(1.3);
+        break;
+      case window.innerWidth<768:
+        setSlidesPerView(2);
+        break;
+      case window.innerWidth<1024:
+        setSlidesPerView(2.5);
+        break;
+      case window.innerWidth<1280:
+        setSlidesPerView(3.3);
+        break;
+      case window.innerWidth<1536:
+        setSlidesPerView(4.5);
+        break;
+      default:
+        setSlidesPerView(5.3);
+  }
+    
+  }, [])
+  
+
   return (
     <div className="drops-section bg-white h-max px-5 py-10">
       <div className="header flex flex-col items-center md:items-start">
@@ -17,9 +50,20 @@ const Drops:FC<DropsProps> = ({ nfts }) => {
         </h1>
         <div className="bg-purple-500 h-1 mt-2 w-64 md:w-96 ml-5"></div>
       </div>
-      <div className="card-container flex md:flex-row flex-col gap-5 h-96">
-        {nfts.map(nft => <DropsItem nft={nft} key={nft.id}/>)}
-      </div>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={slidesPerView}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        className="card-container flex md:flex-row flex-col gap-5 h-96 "
+      >
+        {nfts.map((nft) => (
+          <SwiperSlide>
+            <DropsItem nft={nft} key={nft.id} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
@@ -27,17 +71,17 @@ const Drops:FC<DropsProps> = ({ nfts }) => {
 export default Drops;
 
 interface DropItemsProps {
-  nft: NFT
-};
+  nft: NFT;
+}
 
-const DropsItem:FC<DropItemsProps> = ({ nft }) => {
+const DropsItem: FC<DropItemsProps> = ({ nft }) => {
   return (
     <div className="cont">
       <div className="card1 h-64 w-64 border-solid border-black rounded-lg border-2 mt-8 mb-5 ml-5">
         <h1 className="text-black text-2xl font-bold font-sans m-3 w-36">
           {nft.name}
         </h1>
-        <Image src={nft.image} width='150px' height='150px' alt='NFT Image'/>
+        <Image src={nft.image} width="150px" height="150px" alt="NFT Image" />
         <p className="text-black ml-3">{nft.artist.username}</p>
       </div>
       <button className="self-center text-center py-0.5 rounded bg-purple-500 px-2.5 ml-5">
